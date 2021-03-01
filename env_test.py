cd262d85cf0db27504e46a48e5c2e95fd99ca2f9
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-03-01 23:04:56 +0900

    testing

--- env_test.py
+++ env_test.py
@@ -6,8 +6,8 @@ import numpy as np
 import cv2
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-writer = cv2.VideoWriter('unitytest.mp4',fourcc,60, (80,80))
-writer_ren = cv2.VideoWriter('unitytest_ren.mp4',fourcc,60, (192,192))
+writer = cv2.VideoWriter('unitytest.mp4',fourcc,20, (80,80))
+writer_ren = cv2.VideoWriter('unitytest_ren.mp4',fourcc,20, (192,192))
 
 env_kwargs = dict(
     ip='localhost',
@@ -22,7 +22,8 @@ env.reset()
 # for _ in trange(100):
     # diff += env.check_step(env.action_space.sample())
 for _ in trange(3000):
-    o, r, d, i = env.step(np.array([0.3,np.random.random()*0.1]))
+    direction = float(input())
+    o, r, d, i = env.step(np.array([1.0,direction-2]))
     writer.write(o['obs'][...,-1:-4:-1])
     writer_ren.write(env.render('rgb')[...,::-1])
     if d :
